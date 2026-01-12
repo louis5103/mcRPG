e457397062f53cbb1d4f0ff0e90c2087e45e7ea0
louis5103 <louis38625103@gmail.com>
2026-01-12 19:14:31 +0900

    Initial commit

---
+++ src/main/java/io/github/louis5103/mcRPG/McRPG.java
@@ -0,0 +1,17 @@
+package io.github.louis5103.mcRPG;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class McRPG extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
